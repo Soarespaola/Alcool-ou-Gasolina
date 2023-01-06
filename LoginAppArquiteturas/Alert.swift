a23b58c82abf8d9b04808e286a81e62618b7e727
Paola Soares <paolaalcantara1@yahoo.com.br>
2023-01-06 15:01:57 -0300

    implementa a scene de alerts

---
+++ LoginAppArquiteturas/Alert.swift
@@ -0,0 +1,26 @@
+//
+//  Alert.swift
+//  LoginAppArquiteturas
+//
+//  Created by Paola Alcantara Soares on 06/01/23.
+//
+
+import UIKit
+
+class Alert {
+    
+    let controller: UIViewController
+
+    init(controller: UIViewController) {
+        self.controller =  controller
+    }
+    
+    func showAlertInformation(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "Ok", style: .default)
+        alertController.addAction(okButton)
+        controller.present(alertController, animated: true)
+        
+    }
+    
+}
--- LoginAppArquiteturas/calculator/CalculatorViewController.swift
+++ LoginAppArquiteturas/calculator/CalculatorViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class CalculatorViewController: UIViewController {
     
     var screen: CalculatorScreen?
+    var alert: Alert?
     
     override func loadView() {
         screen = CalculatorScreen()
@@ -19,22 +20,45 @@ class CalculatorViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        alert = Alert.init(controller: self)
         screen?.delegate(delegate: self)
+    }
+    
+    func validadeTextField() -> Bool {
+        
+        guard let ethanoPrice = screen?.ethanolPrice.text else { return false}
+        guard let gasPrice = screen?.gasPrice.text else { return false}
+        
+        if ethanoPrice.isEmpty && gasPrice.isEmpty {
+            alert?.showAlertInformation(title: "Atenção", message: "Infome os valores de Ácool e Gasolina")
+            return false
+        } else if ethanoPrice.isEmpty {
+            alert?.showAlertInformation(title: "Atenção", message: "Infome o valore de Ácool")
+            return false
+        } else if gasPrice.isEmpty {
+            alert?.showAlertInformation(title: "Atenção", message: "Infome o valore de Gasolina")
+            return false
+        }
+        return true
     }
 }
 
 extension CalculatorViewController: CalculatorScreenDelegate {
     func tappedCalculateButton() {
-        let formatter = NumberFormatter()
-        formatter.numberStyle = .decimal
         
-        let ethanolPrice: Double = (formatter.number(from: screen?.ethanolPrice.text ?? "0.0") as? Double) ?? 0.0
-        let gasPrice: Double = (formatter.number(from: screen?.gasPrice.text ?? "0.0") as? Double) ?? 0.0
-        
-        if ethanolPrice / gasPrice > 0.7 {
-            print("Melhor utilizar Gasolina")
-        } else {
-            print("Melhor utilizar Ácool")
+        if validadeTextField() {
+            
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .decimal
+            
+            let ethanolPrice: Double = (formatter.number(from: screen?.ethanolPrice.text ?? "0.0") as? Double) ?? 0.0
+            let gasPrice: Double = (formatter.number(from: screen?.gasPrice.text ?? "0.0") as? Double) ?? 0.0
+            
+            if ethanolPrice / gasPrice > 0.7 {
+                print("Melhor utilizar Gasolina")
+            } else {
+                print("Melhor utilizar Ácool")
+            }
         }
     }
     
